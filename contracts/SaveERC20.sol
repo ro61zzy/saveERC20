ce10f8040a2832f978b8a05c2fddccdd313f982f
rozzy <rosekaremeri@gmail.com>
2024-09-02 21:15:10 +0300

    update imports

--- contracts/SaveERC20.sol
+++ contracts/SaveERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-import "./IERC20.sol";
+import "./interfaces/IERC20.sol";
 
 contract SaveERC20 {
     address owner;
